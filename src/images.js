48a81459ff55c70ff8b3fa81fbb4d0ef55fbafdc
horcrux22 <horcrux22@hotmail.co.kr>
2021-08-25 13:19:33 +0900

    upload files

---
+++ src/images.js
@@ -0,0 +1,11 @@
+import CheckCircleOutline from '../assets/icons/outline_check_box_outline.png';
+import CheckCircle from '../assets/icons/outline_check_box.png';
+import DeleteForever from '../assets/icons/baseline_delete.png';
+import Edit from '../assets/icons/baseline_mode_edit.png';
+
+export const images={
+    uncompleted: CheckCircleOutline,
+    completed: CheckCircle,
+    delete : DeleteForever,
+    update:Edit,
+};
